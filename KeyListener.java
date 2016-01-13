3a2730921ec23b5480005550934b340b6949f949
mikeadkison <mikeadkison@gmail.com>
2016-01-13 14:23:34 -0500

    rebinding a text field's saying now removes the old binding

--- KeyListener.java
+++ KeyListener.java
@@ -14,9 +14,11 @@ public class KeyListener implements HotkeyListener {
 	private final String SCRIPT_PART0 = "' \nset speech = Wscript.CreateObject(\"SAPI.spVoice\")\n speech.speak ";
 	private static int hotKeyIndex = 0; //identifier for registered hot keys
 	private Map<Integer, TextField> hotKeyIndexToSayingFieldMap;
+	private Map<TextField, Integer> sayingFieldToHotKeyIndexMap;
 	
 	public KeyListener() throws IOException {
 		hotKeyIndexToSayingFieldMap = new HashMap<>();
+		sayingFieldToHotKeyIndexMap = new HashMap<>();
 		
 		JIntellitype.setLibraryLocation(new File("JIntellitype64.dll"));
 		JIntellitype.getInstance();
@@ -28,13 +30,20 @@ public class KeyListener implements HotkeyListener {
 		//assign this class to be a HotKeyListener
 		JIntellitype.getInstance().addHotKeyListener(this);
 
-		//Runtime.getRuntime().exec("cmd /c start tosay.vbs");
 	}
 	
 	public void bindToSaying(String character, TextField sayingTextField) {
-		JIntellitype.getInstance().registerHotKey(hotKeyIndex, character);
-		hotKeyIndexToSayingFieldMap.put(hotKeyIndex, sayingTextField);
-		hotKeyIndex++; //increment index so it is unique
+		Integer indexToRebind = sayingFieldToHotKeyIndexMap.get(sayingTextField); //get the uid of the bind coupled with this text field if the text field has been bound before
+		if (null != indexToRebind) {
+			JIntellitype.getInstance().unregisterHotKey(indexToRebind);
+			JIntellitype.getInstance().registerHotKey(indexToRebind, character); //rebind to a new character
+			
+		} else { //no bind exists yet for this text field, so associate the field with a bind (meaning the bind's unique id)
+			JIntellitype.getInstance().registerHotKey(hotKeyIndex, character);
+			sayingFieldToHotKeyIndexMap.put(sayingTextField, hotKeyIndex);
+			hotKeyIndexToSayingFieldMap.put(hotKeyIndex, sayingTextField);
+			hotKeyIndex++; //increment index so it is unique
+		}
 	}
 	
 	// listen for hotkey
